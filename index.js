8b6d9fca46173f665b3a37d4ffaf8d13dca8c392
mrhl082024 <mrhl082024@gmail.com>
2025-01-21 14:57:06 +0100

    done for today

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const buttonBar = document.querySelector("#btn-bar")
+const middleDiv = document.querySelector("#middle-div")
 const categories = ["Creatures", "Equipment", "Materials","Monsters", "Treasure"]
 
 function createButtons() {
@@ -7,36 +8,28 @@ function createButtons() {
         btn.id = "button" + i;
         btn.textContent = categories[i]
         buttonBar.appendChild(btn)
-
-        btn.addEventListener("click", () => {
-           try {
-            const response = fetch(`https://botw-compendium.herokuapp.com/api/v3/compendium/category/${btn.textContent}`);
-            const data = response.json();
-           } catch (error) {
-            throw new Error(error);
-           }
-           const dataName = document.createElement("h1")
-           const dataImg = document.createElement("img")
-           const dataDescription = document.createElement("h3");
-           const dataDrops = document.createElement("h3")
-
-        })
+        btn.addEventListener("click", fetchData(categories[i].toLowerCase()))
     }
 }
-
-async function fetchURL(){
+    
+async function fetchData(category){
     try {
-        const response = await fetch("https://botw-compendium.herokuapp.com/api/v3/compendium/all")
-        const data = await response.json()
-        console.log(data);
-        
-    } catch (error) {
-        throw new Error(error)
-    }
-};
-
-
+        const response = await fetch(`https://botw-compendium.herokuapp.com/api/v3/compendium/category/${category}`);
+        const data = response.json();
+        const dataName = document.createElement("h1")
+        dataName.textContent = data.name;
+        const dataImg = document.createElement("img")
+        dataImg.image = data.image;
+        const dataDescription = document.createElement("h3");
+        dataDescription.textContent = data.description;
+        const dataDrops = document.createElement("h3")
+        dataDrops.textContent = data.drops;
+        middleDiv.appendChild(dataName, dataImg,dataDescription,dataDrops)
+       } catch (error) {
+        throw new Error(error);
+       }
+}
 
 createButtons();
-fetchURL();
+
 
