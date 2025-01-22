49a8824cf650c19d37aba4aa41e5864e7abacc11
mrhl082024 <mrhl082024@gmail.com>
2025-01-22 14:50:27 +0100

    trying to get btn to globalscope...

--- index.js
+++ index.js
@@ -8,28 +8,23 @@ function createButtons() {
         btn.id = "button" + i;
         btn.textContent = categories[i]
         buttonBar.appendChild(btn)
-        btn.addEventListener("click", fetchData(categories[i].toLowerCase()))
+    }
+    return
+};
+
+createButtons() = dataButtons;
+
+async function fetchCategories(){
+    try {
+        const response = await fetch(`https://botw-compendium.herokuapp.com/api/v3/compendium/category/${data.category[i]}`)
+        const data = response.json();
+    } catch (error) {
+        throw new Error(error);
     }
 }
-    
-async function fetchData(category){
-    try {
-        const response = await fetch(`https://botw-compendium.herokuapp.com/api/v3/compendium/category/${category}`);
-        const data = response.json();
-        const dataName = document.createElement("h1")
-        dataName.textContent = data.name;
-        const dataImg = document.createElement("img")
-        dataImg.image = data.image;
-        const dataDescription = document.createElement("h3");
-        dataDescription.textContent = data.description;
-        const dataDrops = document.createElement("h3")
-        dataDrops.textContent = data.drops;
-        middleDiv.appendChild(dataName, dataImg,dataDescription,dataDrops)
-       } catch (error) {
-        throw new Error(error);
-       }
-}
+
+
+
+
 
 createButtons();
-
-
