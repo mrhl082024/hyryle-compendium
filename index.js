e82a08ff7c4559bb5a01b93041f920a54a06b6fd
mrhl082024 <mrhl082024@gmail.com>
2025-01-23 09:14:11 +0100

    made array for buttons, rewrote a bit of code

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
-const buttonBar = document.querySelector("#btn-bar")
+//Old buttons, try to not use, bad practice!
+/* const creatureButton = document.querySelector("#Creatures")
+const equipmentButton = document.querySelector("#Equipment")
+const materialsButton = document.querySelector("#Materials")
+const monstersButton = document.querySelector("#Monsters")
+const treasureButton = document.querySelector("#Treasure") */
+
+const btnBar = document.querySelector("#btn-bar")
+const myButtonArray = ["Creatures", "Equipment", "Materials", "Monsters", "Treasure"]
 const middleDiv = document.querySelector("#middle-div")
-const categories = ["Creatures", "Equipment", "Materials","Monsters", "Treasure"]
 
-function createButtons() {
-    for (let i = 0; i < categories.length; i++) {
-        const btn = document.createElement("button")
-        btn.id = "button" + i;
-        btn.textContent = categories[i]
-        buttonBar.appendChild(btn)
+function createButtons(){
+    for (let i = 0; i < myButtonArray.length; i++) {
+        const button = document.createElement("button");
+        button.id = "button" + i;
+        button.textContent = myButtonArray[i];
+        btnBar.appendChild(button)
     }
-    return
-};
+}
+createButtons()
 
-createButtons() = dataButtons;
 
-async function fetchCategories(){
+async function fetchButtonData(){
     try {
-        const response = await fetch(`https://botw-compendium.herokuapp.com/api/v3/compendium/category/${data.category[i]}`)
-        const data = response.json();
+        const response = await fetch(`https://botw-compendium.herokuapp.com/api/v3/compendium/category/${button.textContent.toLowerCase()}`);
+        const {data} = await response.json();
+        console.log(data);
+        
+        for (let i = 0; i < data.length; i++) {
+            const creatureName = document.createElement("h1");
+            creatureName.textContent = data.name[1];
+
+            const creatureImg = document.createElement("img");
+            creatureImg.src = data.image[1];
+            creatureImg.style.width = 280;
+
+            const creatureDescription = document.createElement("h3");
+            creatureDescription.textContent = data.description[1];
+
+            const creatureDrops = document.createElement("ul");
+            creatureDrops.textContent = data.drops[1];
+        }
+        middleDiv.appendChild(creatureName, creatureImg, creatureDescription, creatureDrops)
+     
     } catch (error) {
         throw new Error(error);
     }
 }
 
-
-
-
-
-createButtons();
