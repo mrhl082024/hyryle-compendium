32d0c0888ac80664e7dbf79f950f91e8938c2595
mrhl082024 <mrhl082024@gmail.com>
2025-01-30 11:38:22 +0100

    HAPPY NOW ALEX?!

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const btnBar = document.querySelector("#btn-bar");
 const containerDiv = document.querySelector("#container");
-const logoImg = document.getElementById("logo-img")
+const logoImg = document.getElementById("logo-img");
 const categoriesArr = [
   "Creatures",
   "Equipment",
@@ -10,8 +10,8 @@ const categoriesArr = [
 ];
 
 logoImg.addEventListener("click", () => {
-  containerDiv.textContent = ""
-}) 
+  containerDiv.textContent = "";
+});
 
 function createButtons() {
   for (let i = 0; i < categoriesArr.length; i++) {
@@ -34,15 +34,74 @@ function createButtons() {
         dataImg.className = "data-img";
         dataImg.src = response.data[i].image;
 
-        const infoCard = document.createElement("div");
-        infoCard.className = "info-card";
-        infoCard.appendChild(dataName);
-        infoCard.appendChild(dataImg);
+        const infoIcon = document.createElement("div");
+        infoIcon.className = "info-card";
+        infoIcon.appendChild(dataName);
+        infoIcon.appendChild(dataImg);
 
-        containerDiv.appendChild(infoCard);
+        containerDiv.appendChild(infoIcon);
 
-        infoCard.addEventListener("click", () => {
-          
+        infoIcon.addEventListener("click", () => {
+          const closeDialog = document.createElement("button");
+          closeDialog.className = "close-dialog-btn";
+          closeDialog.textContent = "Close";
+
+          const infoCard = document.createElement("dialog");
+          infoCard.className = "infoCard";
+
+          const cardName = document.createElement("p");
+          cardName.className = "card-name";
+          cardName.textContent = response.data[i].name;
+
+          const cardImgContainer = document.createElement("div")
+          cardImgContainer.className = "card-img-container";
+          const cardImg = document.createElement("img");
+          cardImg.className = "card-img";
+          cardImg.src = response.data[i].image;
+          cardImgContainer.appendChild(cardImg)
+
+          const cardDescription = document.createElement("p");
+          cardDescription.className = "card-description";
+          cardDescription.textContent = response.data[i].description;
+
+          const cardLocations = document.createElement("ul");
+          cardLocations.className = "card-locations";
+          cardLocations.textContent = "Common Locations: ";
+
+          const locations = document.createElement("li");
+          locations.className = "li-locations"
+          locations.textContent = response.data[i].common_locations;
+          cardLocations.appendChild(locations);
+
+
+          const cardDrops = document.createElement("ul");
+          cardDrops.className = "card-drops";
+          cardDrops.textContent = "Drops: ";
+
+          const drops = document.createElement("li");
+          drops.className = "li-drops"
+          drops.textContent = response.data[i].drops;
+          cardDrops.appendChild(drops);
+
+
+
+          infoCard.appendChild(cardName);
+          infoCard.appendChild(cardImgContainer);
+          infoCard.appendChild(cardDescription);
+          infoCard.appendChild(cardLocations);
+          infoCard.appendChild(cardDrops);
+
+          infoCard.appendChild(closeDialog);
+
+          containerDiv.appendChild(infoCard);
+
+          infoCard.showModal();
+
+          closeDialog.addEventListener("click", () => {
+            infoCard.textContent = "";
+            infoCard.style.display = "none";
+            infoCard.close();
+          });
         });
       }
     });
