68d1dbe11a397f5be0b8e92ecd875eed5110ad09
mrhl082024 <mrhl082024@gmail.com>
2025-01-30 14:40:43 +0100

    redoing some code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const btnBar = document.querySelector("#btn-bar");
 const containerDiv = document.querySelector("#container");
 const logoImg = document.getElementById("logo-img");
+
 const categoriesArr = [
   "Creatures",
   "Equipment",
@@ -12,7 +13,6 @@ const categoriesArr = [
 logoImg.addEventListener("click", () => {
   containerDiv.textContent = "";
 });
-
 
 //testing
 
@@ -28,113 +28,119 @@ async function fetchData(category) {
   }
 }
 
-function loopData(fetchData){
-  const arr= [];
-  for (const [key, actualData] of Object.entries(fetchData)) {
-    arr.push(actualData);
-  }
-  
-}
-
-
-
-
-
-
-
-//testing end
-
 function createButtons() {
   for (let i = 0; i < categoriesArr.length; i++) {
     const button = document.createElement("button");
     button.className = "category-btn";
     button.textContent = categoriesArr[i];
+    btnBar.appendChild(button);
 
     button.addEventListener("click", async () => {
-      containerDiv.textContent = "";
-      const request = await fetch(
-        `https://botw-compendium.herokuapp.com/api/v3/compendium/category/${button.textContent.toLowerCase()}`
-      );
-      const response = await request.json();
-      for (let i = 0; i < response.data.length; i++) {
-        const dataName = document.createElement("p");
-        dataName.className = "data-name";
-        dataName.textContent = response.data[i].name;
-
-        const dataImg = document.createElement("img");
-        dataImg.className = "data-img";
-        dataImg.src = response.data[i].image;
-
-        const infoIcon = document.createElement("div");
-        infoIcon.className = "info-card";
-        infoIcon.appendChild(dataName);
-        infoIcon.appendChild(dataImg);
-
-        containerDiv.appendChild(infoIcon);
-
-        infoIcon.addEventListener("click", () => {
-          const closeDialog = document.createElement("button");
-          closeDialog.className = "close-dialog-btn";
-          closeDialog.textContent = "Close";
-
-          const infoCard = document.createElement("dialog");
-          infoCard.className = "infoCard";
-
-          const cardName = document.createElement("p");
-          cardName.className = "card-name";
-          cardName.textContent = response.data[i].name;
-
-          const cardImgContainer = document.createElement("div");
-          cardImgContainer.className = "card-img-container";
-          const cardImg = document.createElement("img");
-          cardImg.className = "card-img";
-          cardImg.src = response.data[i].image;
-          cardImgContainer.appendChild(cardImg);
-
-          const cardDescription = document.createElement("p");
-          cardDescription.className = "card-description";
-          cardDescription.textContent = response.data[i].description;
-
-          const cardLocations = document.createElement("ul");
-          cardLocations.className = "card-locations";
-          cardLocations.textContent = "Common Locations: ";
-
-          const locations = document.createElement("li");
-          locations.className = "li-locations";
-          locations.textContent = response.data[i].common_locations;
-          cardLocations.appendChild(locations);
-
-          const cardDrops = document.createElement("ul");
-          cardDrops.className = "card-drops";
-          cardDrops.textContent = "Drops: ";
-
-          const drops = document.createElement("li");
-          drops.className = "li-drops";
-          drops.textContent = response.data[i].drops;
-          cardDrops.appendChild(drops);
-
-          infoCard.appendChild(cardName);
-          infoCard.appendChild(cardImgContainer);
-          infoCard.appendChild(cardDescription);
-          infoCard.appendChild(cardLocations);
-          infoCard.appendChild(cardDrops);
-
-          infoCard.appendChild(closeDialog);
-
-          containerDiv.appendChild(infoCard);
-
-          infoCard.showModal();
-
-          closeDialog.addEventListener("click", () => {
-            infoCard.textContent = "";
-            infoCard.style.display = "none";
-            infoCard.close();
-          });
-        });
+      const _data = await fetchData(button.textContent.toLowerCase());
+      const sortedArray = []
+      for (let i = 0; i < _data.length; i++) {
+        
+        
       }
     });
-    btnBar.appendChild(button);
   }
 }
-
 createButtons();
+
+//testing end
+
+// function createButtons() {
+//   for (let i = 0; i < categoriesArr.length; i++) {
+//     const button = document.createElement("button");
+//     button.className = "category-btn";
+//     button.textContent = categoriesArr[i];
+//     btnBar.appendChild(button);
+
+//     //click on a category to load in icons
+//     button.addEventListener("click", async () => {
+//       const data = await fetchData(categoriesArr[i].toLowerCase());
+//       console.log(data);
+
+//       containerDiv.textContent = "";
+
+//       for (let i = 0; i < data.length; i++) {
+//         const dataName = document.createElement("p");
+//         dataName.className = "data-name";
+//         dataName.textContent = response.data[i].name;
+
+//         const dataImg = document.createElement("img");
+//         dataImg.className = "data-img";
+//         dataImg.src = response.data[i].image;
+
+//         const infoIcon = document.createElement("div");
+//         infoIcon.className = "info-card";
+//         infoIcon.appendChild(dataName);
+//         infoIcon.appendChild(dataImg);
+
+//         containerDiv.appendChild(infoIcon);
+
+//         infoIcon.addEventListener("click", () => {
+//           const closeDialog = document.createElement("button");
+//           closeDialog.className = "close-dialog-btn";
+//           closeDialog.textContent = "Close";
+
+//           const infoCard = document.createElement("dialog");
+//           infoCard.className = "infoCard";
+
+//           const cardName = document.createElement("p");
+//           cardName.className = "card-name";
+//           cardName.textContent = response.data[i].name;
+
+//           const cardImgContainer = document.createElement("div");
+//           cardImgContainer.className = "card-img-container";
+//           const cardImg = document.createElement("img");
+//           cardImg.className = "card-img";
+//           cardImg.src = response.data[i].image;
+//           cardImgContainer.appendChild(cardImg);
+
+//           const cardDescription = document.createElement("p");
+//           cardDescription.className = "card-description";
+//           cardDescription.textContent = response.data[i].description;
+
+//           const cardLocations = document.createElement("ul");
+//           cardLocations.className = "card-locations";
+//           cardLocations.textContent = "Common Locations: ";
+
+//           const locations = document.createElement("li");
+//           locations.className = "li-locations";
+//           locations.textContent = response.data[i].common_locations;
+//           cardLocations.appendChild(locations);
+
+//           const cardDrops = document.createElement("ul");
+//           cardDrops.className = "card-drops";
+//           cardDrops.textContent = "Drops: ";
+
+//           const drops = document.createElement("li");
+//           drops.className = "li-drops";
+//           drops.textContent = response.data[i].drops;
+//           cardDrops.appendChild(drops);
+
+//           infoCard.appendChild(cardName);
+//           infoCard.appendChild(cardImgContainer);
+//           infoCard.appendChild(cardDescription);
+//           infoCard.appendChild(cardLocations);
+//           infoCard.appendChild(cardDrops);
+
+//           infoCard.appendChild(closeDialog);
+
+//           containerDiv.appendChild(infoCard);
+
+//           infoCard.showModal();
+
+//           closeDialog.addEventListener("click", () => {
+//             infoCard.textContent = "";
+//             infoCard.style.display = "none";
+//             infoCard.close();
+//           });
+//         });
+//       }
+//     });
+//   }
+// }
+
+// createButtons();
